79afb6e09412cec77c6dfad55d9462f0dcfe4073
lynxjointhepublic <albidaniel34@gmail.com>
2022-07-07 01:38:48 +0700

    delpoyfront

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,12 @@ export default function Document() {
       <Head>
       <div>
   <meta charSet="utf-8" />
-  <title>ProMan - Personal Portfolio HTML Template</title>
+  <title>MyFullMemmory!</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport" />
   <meta content name="keywords" />
   <meta content name="description" />
   {/* Favicon */}
-  <link href="img/favicon.ico" rel="icon" />
+  <link href="img/law.png" rel="icon" />
   {/* Google Web Fonts */}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
